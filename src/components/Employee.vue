f8dfd8e8b67110b0a13346cea499a017afd25aac
pramod subedi <kshitiz184@gmail.com>
2020-05-15 14:04:32 +0545

    checkboxInVueJS

--- src/components/Employee.vue
+++ src/components/Employee.vue
@@ -11,21 +11,35 @@
           <p>
            <label for="officeName">Company Name:</label>
            <input type="text" v-model.lazy="employee.companyName" name="txtCompanyName">  
-         </p>
+          </p>
 
           <p> 
            <label for="mobileNo">mobileNo:</label>
            <input type="text" v-model.lazy="employee.mobileNo" name="txtEmployeemobileNo">
-         </p>
-         <p>
-          <button>Submit</button>
           </p>
+            
+         <p>
+          <label><input type="checkbox" v-model="employee.posts" value="Developer">Developer</label>
+          <label><input type="checkbox" v-model="employee.posts" value="Designer">Designer</label>
+          <label><input type="checkbox" v-model="employee.posts" value="CEO">CEO</label>
+          <label><input type="checkbox" v-model="employee.posts" value="Tester">Tester</label>
+         </p>
+
+         <p>
+        <button>Submit</button>
+        </p>
        </form>
        <div>
           <H3>Employee details preview</H3>
-          <p>Employee name: {{employee.name}}</p>
-          <p>Company name: {{employee.companyName}}</p>
-          <p>Moblie No: {{employee.mobileNo}}</p> 
+          <p>Employee name : {{ employee.name }}</p>
+          <p>Company name  : {{ employee.companyName }}</p>
+          <p>Moblie No     : {{ employee.mobileNo }}</p> 
+           
+           <h3>Employee Occupation:</h3>
+          <ul>
+            <li v-for="post in employee.posts">{{post}}</li>
+          </ul>
+
        </div>
   </div>
 
@@ -41,7 +55,8 @@ export default{
              employee:{
              name:"",
              companyName:"",
-             mobileNo:""
+             mobileNo:"",
+             posts:[]
            }  
     }
   },
