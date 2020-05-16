c24483616427a7441b7457174f5a3457f0d0f04e
pramod subedi <kshitiz184@gmail.com>
2020-05-16 19:56:21 +0545

    HTTP_GETmethod

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,27 @@
 <template>
 
   <div>
-  
-       <h3>Employee form:</h3>
-       <form action="">
-         <p> 
-           <label for="title">Title:</label>
-           <input type="text" v-model.lazy="blog.title">
-         </p>
-
-          <p>
-           <label for="description">Description:</label>
-           <input type="text" v-model.lazy="blog.description">  
-          </p>
-      
-         <p>
-          <button v-on:click.prevent="postBlog">submit</button>
-        </p>
-       </form>
-       <div>
-          <H3> Details preview</H3>
-          <p>Title : {{ blog.title }}</p>
-          <p>Description  : {{ blog.description}}</p>
-          
-
-        </div>    
-
+    <show-blogs></show-blogs>
+     
   </div>
 </template>
 
 <script>
+import ShowBlogs from './components/ShowBlogs.vue'
 
 export default{
-
+components:{
+"show-blogs":ShowBlogs
+},
   data() {
     return {  
-         blog:{
-              title:'',    
-              description:'',
-              user:''            
-           },
+
     }
   },
   methods:
   {
-      postBlog:function()
-    {
-      this.$http.post("https://jsonplaceholder.typicode.com/posts",{
-        title:this.blog.title,
-        body:this.blog.description,
-        userID: this.blog.user
-      }).then(function(data)
-      {
-        console.log(data);
-      });
-    }
+      
+   
   }
 
 }
---
+++ src/components/ShowBlogs.vue
@@ -0,0 +1,37 @@
+<template>
+
+  <div>
+   <h3>Listing Blogs:</h3>
+       <div v-for="blog in blogs">
+              <h3>{{blog.title}}</h3>
+              <p>{{blog.body}}</p>
+       </div>
+  </div>
+</template>
+
+<script>
+export default{
+ 
+  data () { 
+    return {  
+       blogs:[]
+    }
+  },
+ 
+   created(){
+   this.$http.get("https://jsonplaceholder.typicode.com/posts").then(function(data)
+      {
+          console.log(data)
+        this.blogs=data.body.slice(0,5)
+      });
+   }
+}
+
+</script>
+
+<style scoped>
+h3{
+  color:red;
+  font-size:30px
+}
+</style>
